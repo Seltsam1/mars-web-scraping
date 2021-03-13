e613a00ddd1308536c55d981f076440393bfb0a9
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-03-13 05:49:30 -0800

    Update app.py

--- web-scraping/app.py
+++ web-scraping/app.py
@@ -7,6 +7,9 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 
+# From mars-scraping.py file
+import mars_scraping
+
 
 #################################################
 # Flask Setup
@@ -28,6 +31,17 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def index():
     return "TBD"
 
+# Route for scraping websites
+@app.route("/scrape")
+def scrape():
+
+    # Create collection in mongo
+    mars = mongo.db.mars
+    
+    # Call scrape_all function from python file
+    mars_data = mars_scraping.scrape_all()
+
+    return "Scrape is working"
 
 
 
