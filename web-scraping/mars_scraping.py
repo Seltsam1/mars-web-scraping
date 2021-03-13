e90165e9caee30003fc606683691cf99752e26d3
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-03-13 05:49:00 -0800

    Update mars_scraping.py
    Added function featured

--- web-scraping/mars_scraping.py
+++ web-scraping/mars_scraping.py
@@ -25,7 +25,8 @@ def scrape_all():
 
     data = {
         "news_title": news_title,
-        "news_paragraph": news_paragraph
+        "news_paragraph": news_paragraph,
+        "featured_image": featured(browser),
     }
 
     # Close browser
@@ -33,9 +34,6 @@ def scrape_all():
 
     # return data as dictionary
     return data
-
-    # call featured function
-    # featured(browser)
 
 
 ### NASA website ###
@@ -73,114 +71,111 @@ def mars_news(browser):
     return title, paragraph
 
 
-def featured(browser):
-    return None
-
-def mars_fact():
-    return None
-
-def hemisphere():
-    return None
-
-
-
-
-
-
 ### JPL website ###
+def featured(browser):
+
+    # Url to be scraped for image (Jet Propulsion Laboratory, California Institute of Technology)
+    url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+
+    # Direct browser to JPL page
+    browser.visit(url)
+
+    # Save html from browser to variable
+    html = browser.html
+
+    # Create Beautiful soup object and parse
+    soup_image = bs(html, "html.parser")
+
+    # Click on "full image" to get larger pic
+    try:
+        full_image = soup_image.find("a", class_="showimg fancybox-thumbs")
+    except:
+        print("No data found")
+        return None, None
+
+    # Save relative url of image with base url into variable
+    featured = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/" + full_image["href"]
+
+    # Print to console
+    print("Scraping of JPL website complete")
+
+    return featured
 
 
-# Url to be scraped for image (Jet Propulsion Laboratory, California Institute of Technology)
-url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
 
-# Direct browser to JPL page
-browser.visit(url)
+# def mars_fact():
+#     return None
 
-# Save html from browser to variable
-html = browser.html
-
-# Create Beautiful soup object and parse
-soup_image = bs(html, "html.parser")
-
-# Click on "full image" to get larger pic
-full_image = soup_image.find("a", class_="showimg fancybox-thumbs")
-
-# Save relative url of image with base url into variable
-featured = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/" + full_image["href"]
-
-# Print to console
-print("Scraping of JPL website complete")
+# def hemisphere():
+#     return None
 
 
-### Space Facts website ###
+
+# ### Space Facts website ###
 
 
-# Convert html with pandas
-df = pd.read_html("https://space-facts.com/mars/")
+# # Convert html with pandas
+# df = pd.read_html("https://space-facts.com/mars/")
 
-# Select first dataframe in list
-mars_df = df[0]
+# # Select first dataframe in list
+# mars_df = df[0]
 
-### Cleaning dataframe
+# ### Cleaning dataframe
 
-# Name columns
-mars_df.columns=["Description", "Mars"]
+# # Name columns
+# mars_df.columns=["Description", "Mars"]
 
-# Set first column as index
-mars_df.set_index("Description", inplace=True)
+# # Set first column as index
+# mars_df.set_index("Description", inplace=True)
 
-# Convert dataframe to html table
-mars_table = mars_df.to_html()
+# # Convert dataframe to html table
+# mars_table = mars_df.to_html()
 
-# Print to console
-print("Scraping of Space Facts website complete")
-
-
-### USGS Astrogeology website ###
+# # Print to console
+# print("Scraping of Space Facts website complete")
 
 
-# Url to be scraped for images (USBS Astrogeology)
+# ### USGS Astrogeology website ###
 
-url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
-# Direct browser to USGS page
-browser.visit(url)
+# # Url to be scraped for images (USBS Astrogeology)
 
-# List of hemispheres
-hemisphere_urls = browser.find_by_css("a.product-item h3")
+# url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
-# Empty list to save final pic urls
-hemisphere_images = []
+# # Direct browser to USGS page
+# browser.visit(url)
 
-# Loop through list of hemispheres
-for i in range(len(hemisphere_urls)):
+# # List of hemispheres
+# hemisphere_urls = browser.find_by_css("a.product-item h3")
+
+# # Empty list to save final pic urls
+# hemisphere_images = []
+
+# # Loop through list of hemispheres
+# for i in range(len(hemisphere_urls)):
     
-    # Empty dictionary for images
-    hemisphere = {}
+#     # Empty dictionary for images
+#     hemisphere = {}
     
-    # Click to get link to larger image
-    browser.find_by_css("a.product-item h3")[i].click()
+#     # Click to get link to larger image
+#     browser.find_by_css("a.product-item h3")[i].click()
     
-    # Get image url and titles for images
-    hemisphere_links = browser.links.find_by_text("Sample").first["href"]
-    hemisphere_title = browser.find_by_css("h2.title").text
+#     # Get image url and titles for images
+#     hemisphere_links = browser.links.find_by_text("Sample").first["href"]
+#     hemisphere_title = browser.find_by_css("h2.title").text
     
-    # Save results in dictionary
-    hemisphere["title"] = hemisphere_title
-    hemisphere["link"] = hemisphere_links
+#     # Save results in dictionary
+#     hemisphere["title"] = hemisphere_title
+#     hemisphere["link"] = hemisphere_links
     
-    # Append dictionary values to list
-    hemisphere_images.append(hemisphere)
+#     # Append dictionary values to list
+#     hemisphere_images.append(hemisphere)
     
-    # Go back to prior page
-    browser.back()
+#     # Go back to prior page
+#     browser.back()
 
-# Print results to console
-# Print to console
-print("Scraping of USGS Astrogeology complete")
-    
+# # Print results to console
+# # Print to console
+# print("Scraping of USGS Astrogeology complete")
 
-### Close browser ###
-
-browser.quit()
 
